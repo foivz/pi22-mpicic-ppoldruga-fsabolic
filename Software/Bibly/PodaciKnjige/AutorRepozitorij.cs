7950894e06dc6a4b1275526737345fa1bf614bb5
MartaMarija <mpicic@foi.hr>
2022-06-22 21:09:46 +0200

    Metoda DohvatiSveKnjige()

--- Software/Bibly/PodaciKnjige/AutorRepozitorij.cs
+++ Software/Bibly/PodaciKnjige/AutorRepozitorij.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Baza;
 
 namespace PodaciKnjige
 {
     public class AutorRepozitorij
     {
+        public List<Autor> DohvatiAutoreKnjige(string ISBN)
+        {
+            BazaPodataka.Instanca.UspostaviVezu();
+
+            string upit =
+                    "SELECT k.ISBN AS 'k.ISBN'" +
+                    ", k.naziv AS 'k.naziv'" +
+                    ", k.id_izdavac AS 'k.id_izdavac'" +
+                    ", k.id_zanr AS 'k.id_zanr'" +
+                    ", k.datum_izdavanja AS 'k.datum_izdavanja'" +
+                    ", k.broj_stranica AS 'k.broj_stranica'" +
+                    ", k.opis_knjige AS 'k.opis_knjige'" +
+                    ", k.naslovnica AS 'k.naslovnica'" +
+                    ", i.id_izdavac AS 'i.id_izdavac'" +
+                    ", i.naziv AS 'i.naziv'" +
+                    ", z.id_zanr AS 'z.id_zanr'" +
+                    ", z.naziv AS 'z.naziv'" +
+                    " FROM knjige k" +
+                    " JOIN izdavaci i" +
+                    " ON i.id_izdavac = k.id_izdavac" +
+                    " JOIN zanrovi z" +
+                    " ON z.id_zanr = k.id_zanr";
+
+            List<Autor> autori = new List<Autor>();
+
+            IDataReader reader = BazaPodataka.Instanca.DohvatiDataReader(upit);
+            while (reader.Read())
+            {
+                autori.Add(new Autor(
+                   reader["k.ISBN"].ToString(),
+                   reader["k.naziv"].ToString(),
+                   new Izdavac(
+                        int.Parse(reader["i.id_izdavac"].ToString()),
+                        reader["i.naziv"].ToString()
+                        ),
+                   new Zanr(
+                        int.Parse(reader["z.id_zanr"].ToString()),
+                        reader["z.naziv"].ToString()
+                        ),
+                    DateTime.Parse(reader["k.datum_izdavanja"].ToString()),
+                    int.Parse(reader["k.broj_stranica"].ToString()),
+                    reader["k.opis_knjige"].ToString(),
+                    reader["k.naslovnica"].ToString(),
+                    null
+                   ));
+            }
+            reader.Close();
+
+            BazaPodataka.Instanca.PrekiniVezu();
+
+            return autori;
+        } 
     }
 }
--- Software/Bibly/PodaciKnjige/KnjigaRepozitorij.cs
+++ Software/Bibly/PodaciKnjige/KnjigaRepozitorij.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 using Baza;
 
 namespace PodaciKnjige
@@ -11,9 +13,57 @@ namespace PodaciKnjige
     {
         public List<Knjiga> DohvatiSveKnjige()
         {
-            List<Knjiga> knjiga = new List<Knjiga>();
-            return knjiga;
+            BazaPodataka.Instanca.UspostaviVezu();
+
+            string upit =
+                    "SELECT k.ISBN AS 'k.ISBN'" +
+                    ", k.naziv AS 'k.naziv'" +
+                    ", k.id_izdavac AS 'k.id_izdavac'" +
+                    ", k.id_zanr AS 'k.id_zanr'" +
+                    ", k.datum_izdavanja AS 'k.datum_izdavanja'" +
+                    ", k.broj_stranica AS 'k.broj_stranica'" +
+                    ", k.opis_knjige AS 'k.opis_knjige'" +
+                    ", k.naslovnica AS 'k.naslovnica'" +
+                    ", i.id_izdavac AS 'i.id_izdavac'" +
+                    ", i.naziv AS 'i.naziv'" +
+                    ", z.id_zanr AS 'z.id_zanr'" +
+                    ", z.naziv AS 'z.naziv'" +
+                    " FROM knjige k" +
+                    " JOIN izdavaci i" +
+                    " ON i.id_izdavac = k.id_izdavac" +
+                    " JOIN zanrovi z" +
+                    " ON z.id_zanr = k.id_zanr";
+
+            List<Knjiga> knjige = new List<Knjiga>();
+
+            IDataReader reader = BazaPodataka.Instanca.DohvatiDataReader(upit);
+            while (reader.Read())
+            {
+                knjige.Add(new Knjiga(
+                   reader["k.ISBN"].ToString(),
+                   reader["k.naziv"].ToString(),
+                   new Izdavac(
+                        int.Parse(reader["i.id_izdavac"].ToString()),
+                        reader["i.naziv"].ToString()
+                        ),
+                   new Zanr(
+                        int.Parse(reader["z.id_zanr"].ToString()),
+                        reader["z.naziv"].ToString()
+                        ),
+                    DateTime.Parse(reader["k.datum_izdavanja"].ToString()),
+                    int.Parse(reader["k.broj_stranica"].ToString()),
+                    reader["k.opis_knjige"].ToString(),
+                    reader["k.naslovnica"].ToString(),
+                    null
+                   ));
+            }
+            reader.Close();
+
+            BazaPodataka.Instanca.PrekiniVezu();
+
+            return knjige;
         }
+
         public List<Knjiga> DohvatiNajcitanijeKnjige()
         {
             List<Knjiga> knjiga = new List<Knjiga>();
