e3e6b4b70d767296dbaa9441299f4f5c8ea571d9
MartaMarija <mpicic@foi.hr>
2022-06-22 19:49:00 +0200

    Knjige
    Dodani nazivi metoda u KnjigaRepozitorij i napravljena klasa Knjiga

--- Software/Bibly/PodaciKnjige/Knjiga.cs
+++ Software/Bibly/PodaciKnjige/Knjiga.cs
@@ -8,5 +8,32 @@ namespace PodaciKnjige
 {
     public class Knjiga
     {
+        public string ISBN { get; set; }
+        public string Naziv { get; set; }
+        public Izdavac Izdavac { get; set; }
+        public Zanr Zanr { get; set; }
+        public DateTime DatumIzdavanja { get; set; }
+        public int BrojStranica { get; set; }
+        public string Opis { get; set; }
+        public string Naslovnica { get; set; }
+        public List<Autor> Autor { get; set; }
+
+        public Knjiga()
+        {
+             
+        }
+
+        public Knjiga(string isbn, string naziv, Izdavac izdavac, Zanr zanr, DateTime datumIzdavanja, int brojStranica, string opis, string naslovnica, List<Autor> autori)
+        {
+            ISBN = isbn;
+            Naziv = naziv;
+            Izdavac = izdavac;
+            Zanr = zanr;
+            DatumIzdavanja = datumIzdavanja;
+            BrojStranica = brojStranica;
+            Opis = opis;
+            Naslovnica = naslovnica;
+            Autor = autori;
+        }
     }
 }
--- Software/Bibly/PodaciKnjige/KnjigaRepozitorij.cs
+++ Software/Bibly/PodaciKnjige/KnjigaRepozitorij.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Baza;
 
 namespace PodaciKnjige
 {
     public class KnjigaRepozitorij
     {
+        public List<Knjiga> DohvatiSveKnjige()
+        {
+            List<Knjiga> knjiga = new List<Knjiga>();
+            return knjiga;
+        }
+        public List<Knjiga> DohvatiNajcitanijeKnjige()
+        {
+            List<Knjiga> knjiga = new List<Knjiga>();
+            return knjiga;
+        }
+        public int DodajKnjigu(Knjiga dodanaKnjiga)
+        {
+            return 0;
+        }
+        public int ObrisiKnjigu(Knjiga dodanaKnjiga)
+        {
+            return 0;
+        }
+        public int AzurirajKnjigu(Knjiga dodanaKnjiga)
+        {
+            return 0;
+        }
+        public List<Knjiga> DohvatiKnjiguPoKriteriju()
+        {
+            List<Knjiga> knjiga = new List<Knjiga>();
+            return knjiga;
+        }
     }
 }
