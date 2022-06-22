4206a1ae41a7c8021c0c49e862eea25404e7f39f
MartaMarija <mpicic@foi.hr>
2022-06-23 00:37:19 +0200

    Uređen dizajn UCKnjigeKatalog

--- Software/Bibly/Bibly/UCKnjigaKatalog.Designer.cs
+++ Software/Bibly/Bibly/UCKnjigaKatalog.Designer.cs
@@ -34,12 +34,18 @@
             this.lblBrojStranica = new System.Windows.Forms.Label();
             this.lblOpisKnjige = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblNaslov
             // 
             this.lblNaslov.AutoSize = true;
+            this.lblNaslov.BackColor = System.Drawing.Color.White;
             this.lblNaslov.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNaslov.Location = new System.Drawing.Point(228, 53);
             this.lblNaslov.Name = "lblNaslov";
@@ -50,18 +56,20 @@
             // lblAutor
             // 
             this.lblAutor.AutoSize = true;
-            this.lblAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAutor.Location = new System.Drawing.Point(228, 84);
+            this.lblAutor.BackColor = System.Drawing.Color.White;
+            this.lblAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAutor.Location = new System.Drawing.Point(291, 84);
             this.lblAutor.Name = "lblAutor";
-            this.lblAutor.Size = new System.Drawing.Size(51, 20);
+            this.lblAutor.Size = new System.Drawing.Size(46, 17);
             this.lblAutor.TabIndex = 1;
             this.lblAutor.Text = "label2";
             // 
             // lblIzdavac
             // 
             this.lblIzdavac.AutoSize = true;
+            this.lblIzdavac.BackColor = System.Drawing.Color.White;
             this.lblIzdavac.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIzdavac.Location = new System.Drawing.Point(228, 113);
+            this.lblIzdavac.Location = new System.Drawing.Point(303, 113);
             this.lblIzdavac.Name = "lblIzdavac";
             this.lblIzdavac.Size = new System.Drawing.Size(46, 17);
             this.lblIzdavac.TabIndex = 2;
@@ -70,8 +78,9 @@
             // lblBrojStranica
             // 
             this.lblBrojStranica.AutoSize = true;
+            this.lblBrojStranica.BackColor = System.Drawing.Color.White;
             this.lblBrojStranica.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBrojStranica.Location = new System.Drawing.Point(228, 143);
+            this.lblBrojStranica.Location = new System.Drawing.Point(340, 143);
             this.lblBrojStranica.Name = "lblBrojStranica";
             this.lblBrojStranica.Size = new System.Drawing.Size(46, 17);
             this.lblBrojStranica.TabIndex = 3;
@@ -79,11 +88,11 @@
             // 
             // lblOpisKnjige
             // 
-            this.lblOpisKnjige.AutoSize = true;
+            this.lblOpisKnjige.BackColor = System.Drawing.Color.White;
             this.lblOpisKnjige.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpisKnjige.Location = new System.Drawing.Point(228, 172);
+            this.lblOpisKnjige.Location = new System.Drawing.Point(276, 172);
             this.lblOpisKnjige.Name = "lblOpisKnjige";
-            this.lblOpisKnjige.Size = new System.Drawing.Size(46, 17);
+            this.lblOpisKnjige.Size = new System.Drawing.Size(648, 131);
             this.lblOpisKnjige.TabIndex = 4;
             this.lblOpisKnjige.Text = "label5";
             // 
@@ -95,18 +104,75 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Autori:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Izdavač:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(229, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Broj stranica:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(229, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Opis:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(3, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1069, 299);
+            this.panel1.TabIndex = 10;
+            // 
             // UCKnjigaKatalog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblOpisKnjige);
             this.Controls.Add(this.lblBrojStranica);
             this.Controls.Add(this.lblIzdavac);
             this.Controls.Add(this.lblAutor);
             this.Controls.Add(this.lblNaslov);
+            this.Controls.Add(this.panel1);
             this.Name = "UCKnjigaKatalog";
-            this.Size = new System.Drawing.Size(637, 365);
+            this.Size = new System.Drawing.Size(1099, 353);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -121,5 +187,10 @@
         private System.Windows.Forms.Label lblBrojStranica;
         private System.Windows.Forms.Label lblOpisKnjige;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
