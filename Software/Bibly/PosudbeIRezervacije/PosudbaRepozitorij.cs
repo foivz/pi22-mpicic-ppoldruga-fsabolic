1b2396f39335f452cf5816ead878dfc01c95110e
MartaMarija <mpicic@foi.hr>
2022-06-24 01:01:17 +0200

    Dodavanje posudbe

---
+++ Software/Bibly/PosudbeIRezervacije/PosudbaRepozitorij.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Baza;
+using PodaciKnjige;
+using Prijava;
+
+namespace PosudbeIRezervacije
+{
+    public static class PosudbaRepozitorij
+    {
+
+    }
+}
---
+++ Software/Bibly/PosudbeIRezervacije/RezervacijaRepozitorij.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Baza;
+using PodaciKnjige;
+using Prijava;
+
+namespace PosudbeIRezervacije
+{
+    public static class RezervacijaRepozitorij
+    {
+        public static int DodajRezervaciju(Posudba rezervacija)
+        {
+            BazaPodataka.Instanca.UspostaviVezu();
+
+            string upit =
+                    "INSERT INTO posudbe " +
+                       "(id_primjerak" +
+                       ", id_korisnik" +
+                       ", do_kada_vrijedi_rezervacija" +
+                       ", rezervacija_potvrdena)" +
+                    " VALUES" +
+                        "(" + rezervacija.Primjerak.Id +
+                        ", '" + rezervacija.Korisnik.OIB + "'" +
+                        ", " + rezervacija.DoKadaVrijediRezervacija.ToShortDateString() +
+                        ", " + rezervacija.RezervacijaPotvrdena +
+                        ")";
+
+            BazaPodataka.Instanca.PrekiniVezu();
+
+            return 0;
+        }
+    }
+}
