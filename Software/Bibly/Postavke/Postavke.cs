00694ef6dfff67314a53f65b09a2230bca45517a
MartaMarija <mpicic@foi.hr>
2022-06-24 01:01:45 +0200

    Dodavanje postavki

---
+++ Software/Bibly/Postavke/Postavke.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postavke
+{
+    public class Postavke
+    {
+        private static Postavke instanca;
+        public static Postavke Instanca
+        {
+            get
+            {
+                if (instanca == null)
+                    instanca = new Postavke();
+                return instanca;
+            }
+        }
+    }
+}
