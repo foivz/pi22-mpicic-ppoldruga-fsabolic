7fd17a07f6537eabaa767c092cc36214edc162aa
MartaMarija <mpicic@foi.hr>
2022-06-23 12:53:23 +0200

    Dodani primjerak i repozitorij primjerka

---
+++ Software/Bibly/PodaciKnjige/Primjerak.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodaciKnjige
+{
+    public class Primjerak
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public StatusPrimjerka Status { get; set; }
+        public Knjiga Knjiga { get; set; }
+        public Primjerak(int id, string isbn, StatusPrimjerka status, Knjiga knjiga)
+        {
+
+        }
+    }
+}
