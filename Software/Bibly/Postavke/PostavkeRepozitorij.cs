00312efadfa02fed7c1e080a1c04f587f1ee1cdb
MartaMarija <mpicic@foi.hr>
2022-06-24 16:07:08 +0200

    Zamjena Postavke.cs u PostavkeRepozitorij
    Postoji samo jedna instanca postavki pa možemo odmah podatke iz repozitorija dohvaćati.

---
+++ Software/Bibly/Postavke/PostavkeRepozitorij.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postavke
+{
+    public static class PostavkeRepozitorij
+    {
+        public static int VratiTrajanjeRezervacije()
+        {
+            return 0;
+        }
+    }
+}
