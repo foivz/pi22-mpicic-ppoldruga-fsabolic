01160a8ca9052d462e887d23a981c551bea0cdcd
MartaMarija <mpicic@foi.hr>
2022-06-24 16:08:36 +0200

    Dodavanje Baza u Postavke

--- Software/Bibly/Postavke/PostavkeRepozitorij.cs
+++ Software/Bibly/Postavke/PostavkeRepozitorij.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Baza;
 
 namespace Postavke
 {
@@ -10,6 +11,7 @@ namespace Postavke
     {
         public static int VratiTrajanjeRezervacije()
         {
+
             return 0;
         }
     }
