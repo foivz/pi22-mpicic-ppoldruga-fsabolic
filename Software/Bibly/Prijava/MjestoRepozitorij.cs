60f6506fad54aaf8dd38070cc81865198b1d9874
fsabolic <fransabolicm8@gmail.com>
2022-06-27 23:47:29 +0200

    Dodan repozitorij za Mjesto

---
+++ Software/Bibly/Prijava/MjestoRepozitorij.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baza;
+
+namespace Prijava
+{
+    public static class MjestoRepozitorij
+    {
+
+        public static List<Mjesto> DohvatiSvaMjesta()
+        {
+            BazaPodataka.Instanca.UspostaviVezu();
+
+            string upit =
+                   "SELECT * FROM mjesta ORDER BY naziv";
+
+
+            List<Mjesto> mjesta = new List<Mjesto>();
+
+            IDataReader reader = BazaPodataka.Instanca.DohvatiDataReader(upit);
+            while (reader.Read())
+            {
+                mjesta.Add(new Mjesto(
+                  int.Parse(reader["id_mjesto"].ToString()),
+                  reader["naziv"].ToString()
+                   ));
+
+
+            }
+            reader.Close();
+
+            BazaPodataka.Instanca.PrekiniVezu();
+
+            return mjesta;
+        }
+    }
+}
---
+++ Software/Bibly/Prijava/TipKorisnikaRepozitorij.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Baza;
+using System.Data;
+namespace Prijava
+{
+    public static class TipKorisnikaRepozitorij
+    {
+        public static List<TipKorisnika> DohvatiSveTipoveKorisnika()
+        {
+            BazaPodataka.Instanca.UspostaviVezu();
+
+            string upit =
+                   "SELECT * FROM tipovi_korisnika";
+
+
+            List<TipKorisnika> tipoviKorisnika = new List<TipKorisnika>();
+
+            IDataReader reader = BazaPodataka.Instanca.DohvatiDataReader(upit);
+            while (reader.Read())
+            {
+                tipoviKorisnika.Add(new TipKorisnika(
+                  int.Parse(reader["id_tip_korisnika"].ToString()),
+                  reader["naziv"].ToString()
+                   ));
+
+
+            }
+            reader.Close();
+
+            BazaPodataka.Instanca.PrekiniVezu();
+
+            return tipoviKorisnika;
+        }
+    }
+}
