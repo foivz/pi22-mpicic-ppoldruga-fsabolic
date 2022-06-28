8ac9b5714bf28d439b16bcc634e3efbf63abfd06
fsabolic <fransabolicm8@gmail.com>
2022-06-28 14:32:10 +0200

    Dohvaćene sve vrijednosti autora iz AutorRepozitorij

--- Software/Bibly/PodaciKnjige/AutorRepozitorij.cs
+++ Software/Bibly/PodaciKnjige/AutorRepozitorij.cs
@@ -16,8 +16,11 @@ namespace PodaciKnjige
             BazaPodataka.Instanca.UspostaviVezu();
 
             string upit =
-                    "SELECT a.ime AS 'a.ime'" +
+                    "SELECT " +
+                    "a.id_autor AS 'a.id_autor'"+
+                    ",a.ime AS 'a.ime'" +
                     ", a.prezime AS 'a.prezime'" +
+                    ",a.biografija AS 'a.biografija'"+
                     " FROM autori a" +
                     " JOIN autor_knjige ak" +
                     " ON a.id_autor = ak.id_autor" +
@@ -31,8 +34,10 @@ namespace PodaciKnjige
             while (reader.Read())
             {
                 autori.Add(new Autor(
+                    int.Parse(reader["a.id_autor"].ToString()),
                    reader["a.ime"].ToString(),
-                   reader["a.prezime"].ToString()
+                   reader["a.prezime"].ToString(),
+                   reader["a.biografija"].ToString()
                    ));
             }
             reader.Close();
@@ -40,6 +45,33 @@ namespace PodaciKnjige
             BazaPodataka.Instanca.PrekiniVezu();
 
             return autori;
-        } 
+        }
+
+        public static List<Autor> DohvatiSveAutore()
+        {
+            BazaPodataka.Instanca.UspostaviVezu();
+
+            string upit =
+                    "SELECT * FROM autori";
+
+            List<Autor> autori = new List<Autor>();
+
+            IDataReader reader = BazaPodataka.Instanca.DohvatiDataReader(upit);
+            while (reader.Read())
+            {
+                autori.Add(new Autor(
+                    int.Parse(reader["id_autor"].ToString()),
+                   reader["ime"].ToString(),
+                   reader["prezime"].ToString(),
+                   reader["biografija"].ToString()
+
+                   ));
+            }
+            reader.Close();
+
+            BazaPodataka.Instanca.PrekiniVezu();
+
+            return autori;
+        }
     }
 }
