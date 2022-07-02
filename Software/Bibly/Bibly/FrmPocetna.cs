5252e80a31bc17a00984269962c1125f63f57c7b
ppoldrugac <92984418+ppoldrugac@users.noreply.github.com>
2022-07-02 23:09:50 +0200

    Kreirana forma koja će biti početna stranica.

---
+++ Software/Bibly/Bibly/FrmPocetna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bibly
+{
+    public partial class FrmPocetna : FrmOpcenita
+    {
+        public FrmPocetna()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPocetna_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
