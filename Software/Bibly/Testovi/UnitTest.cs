4c08209ecc10e02292a2a625eb1230dcbfb952db
MartaMarija <mpicic@foi.hr>
2022-07-03 18:42:24 +0200

    Dodan test

--- Software/Bibly/Testovi/UnitTest.cs
+++ Software/Bibly/Testovi/UnitTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using PosudbeIRezervacije;
+using Postavke;
 
 namespace Testovi
 {
@@ -7,8 +9,22 @@ namespace Testovi
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IzracunajZakasninu_ProsaoDatumVracanja_VracaZakasninu()
         {
+            //Arrange
+            Posudba posudba = new Posudba
+            {
+                Id = 60,
+                PredvideniDatumVracanja = DateTime.Now.AddDays(-2).Date
+            };
+
+            //Act
+            double zakasnina = posudba.IzracunajZakasninu();
+
+            //Assert
+            double cijenaZakasnineJednogDana = PostavkeRepozitorij.DohvatiIznosZakasnine();
+            double ispravnaZaksnina = 2 * cijenaZakasnineJednogDana;
+            Assert.AreEqual(ispravnaZaksnina, zakasnina);
         }
     }
 }
