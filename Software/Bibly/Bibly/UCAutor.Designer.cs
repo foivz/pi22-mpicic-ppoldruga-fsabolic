78bce8e9cbdd168d5a8d5b567100c4d36f25f3fe
ppoldrugac <92984418+ppoldrugac@users.noreply.github.com>
2022-07-06 12:28:31 +0200

    Kreirana User Controla UCAutor.

---
+++ Software/Bibly/Bibly/UCAutor.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Bibly
+{
+    partial class UCAutor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbAutor = new System.Windows.Forms.ComboBox();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbAutor
+            // 
+            this.cmbAutor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAutor.FormattingEnabled = true;
+            this.cmbAutor.Items.AddRange(new object[] {
+            "Ime i prezime korisnika",
+            "Id primjeraka",
+            "Naziv knjige"});
+            this.cmbAutor.Location = new System.Drawing.Point(177, 33);
+            this.cmbAutor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbAutor.Name = "cmbAutor";
+            this.cmbAutor.Size = new System.Drawing.Size(361, 37);
+            this.cmbAutor.TabIndex = 35;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(231)))), ((int)(((byte)(227)))));
+            this.lblAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAutor.Location = new System.Drawing.Point(22, 36);
+            this.lblAutor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(122, 44);
+            this.lblAutor.TabIndex = 34;
+            this.lblAutor.Text = "Autor:";
+            // 
+            // UCAutor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(231)))), ((int)(((byte)(227)))));
+            this.Controls.Add(this.cmbAutor);
+            this.Controls.Add(this.lblAutor);
+            this.Name = "UCAutor";
+            this.Size = new System.Drawing.Size(573, 99);
+            this.Load += new System.EventHandler(this.UCAutor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbAutor;
+        private System.Windows.Forms.Label lblAutor;
+    }
+}
