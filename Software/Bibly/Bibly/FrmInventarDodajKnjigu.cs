d2ee0a70b22d995a752ace50cf1ee4f9a813fc40
ppoldrugac <92984418+ppoldrugac@users.noreply.github.com>
2022-07-07 12:35:39 +0200

    Dodane provjera unosa i validacija ISBN-a.

--- Software/Bibly/Bibly/FrmInventarDodajKnjigu.cs
+++ Software/Bibly/Bibly/FrmInventarDodajKnjigu.cs
@@ -24,6 +24,7 @@ namespace Bibly
             PopuniComboBoxeve();
             cmbIzdavac.SelectedIndex = 0;
             cmbZanr.SelectedIndex = 0;
+
             if (OdabranaKnjiga != null)
             {
                 ((TextBox)this.Controls.Find("txtISBN", true)[0]).Text = odabranaKnjiga.ISBN.ToString();
@@ -36,6 +37,8 @@ namespace Bibly
                 ((TextBox)this.Controls.Find("txtBrojStranica", true)[0]).Text = odabranaKnjiga.BrojStranica.ToString();
                 ((TextBox)this.Controls.Find("txtOpis", true)[0]).Text = odabranaKnjiga.Opis.ToString();
                 ((PictureBox)this.Controls.Find("pbNaslovnica", true)[0]).Image = odabranaKnjiga.Naslovnica;
+
+                
             }
 
         }
@@ -126,7 +129,77 @@ namespace Bibly
 
         private void btnSpremiKnjigu_Click(object sender, EventArgs e)
         {
+            string isbn = txtISBN.Text;
+            string naziv = txtISBN.Text;
+            //int brojStranica = int.Parse(txtBrojStranica.Text.ToString());
+            //DateTime datumIzdavanja = DateTime.Parse(DateTime.Parse(txtDatumIzdavanja.Text).Date.ToString("yyyy-MM-dd"));
+            string opisKnjige = txtOpis.Text;
+            Image naslovnica = pbNaslovnica.Image;
+
+
+            int dobroPopunjeno = ProvjeriUnose(isbn, naziv, txtBrojStranica.Text.ToString(), txtDatumIzdavanja.Text.ToString(), opisKnjige,
+                cmbIzdavac.Text.ToString(), cmbZanr.Text.ToString());
+            string poruka = "";
+            switch (dobroPopunjeno)
+            {
+                case -1:
+                    poruka = "Niste popunili sva polja!";
+                    break;
+                case -2:
+                    poruka = "ISBN je pogrešnog formata. Mora sadržavati 13 znakova!";
+                    break;
+                case -3:
+                    poruka = "Ovaj ISBN već postoji!";
+                    break;
+            }
+
+            if (dobroPopunjeno != 1)
+            {
+                MessageBox.Show(poruka);
+            }
 
         }
+
+        private int ProvjeriUnose(string isbn, string naziv, string brStr, string datum, string opis, string izdavac, string zanr)
+        {
+            if (string.IsNullOrEmpty(isbn) || string.IsNullOrWhiteSpace(isbn) || string.IsNullOrEmpty(naziv) || string.IsNullOrWhiteSpace(naziv) ||
+                string.IsNullOrEmpty(brStr) || string.IsNullOrWhiteSpace(brStr) || string.IsNullOrEmpty(datum) || string.IsNullOrWhiteSpace(datum) ||
+                string.IsNullOrEmpty(opis) || string.IsNullOrWhiteSpace(opis) || string.IsNullOrEmpty(izdavac) || string.IsNullOrWhiteSpace(izdavac) ||
+                string.IsNullOrEmpty(zanr) || string.IsNullOrWhiteSpace(zanr) )
+            {
+                return -1;
+            }
+            else if (!ProvjeraISBN(isbn))
+            {
+                return -2;
+            }
+            else if (!PostojiISBN(isbn))
+            {
+                return -3;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private bool PostojiISBN(string isbn)
+        {
+            if (OdabranaKnjiga == null)
+            {
+                if (KnjigaRepozitorij.DohvatiKnjigu(isbn) != null) return false;
+            } 
+            return true;
+        }
+
+        private bool ProvjeraISBN(string isbn)
+        {
+            if (isbn.Length != 13)
+            {
+                return false;
+            }
+            else return true;
+        }
     }
+
 }
